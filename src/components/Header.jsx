42c0554957f08761164624ffc779eda07ee85b48
Daniel Lee <daniejlee@gmail.com>
2021-04-22 23:46:06 -0700

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 
-export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
+const Header = () => {
 
-  render() {
+
     return (
       <nav className="navbar navbar-dark navcolor justify-content-between py-1">
-        <div className="navbar-brand header cursor-pointer" id="home" onClick={this.handleClick}>
-        </div>
+        <div className="navbar-brand header cursor-pointer" id="home">
+          HELLO
         </div>
       </nav>
     );
-  }
 }
+
+
+export default Header
