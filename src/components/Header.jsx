23a82503ed269cca7c278f8b1b149e1c87ccc0b7
Daniel Lee <daniejlee@gmail.com>
2021-04-23 23:44:18 -0700

    bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,10 @@ const Header = () => {
     return (
       <nav className="navbar navbar-dark navcolor justify-content-between py-1">
         <div className="navbar-brand header cursor-pointer" id="home">
-          HELLO
+          {' '} Hi-Fi Planet
+        </div>
+        <div className="cart" id="cart">
+          <i className="fas fa-shopping-cart ml-2"></i>
         </div>
       </nav>
     );
